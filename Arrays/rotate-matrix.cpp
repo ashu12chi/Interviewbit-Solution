eb61b20850183f17d3504b795befa1247ed4e65e
Ashutosh Chitranshi <45908393+ashu12chi@users.noreply.github.com>
2020-06-28 04:39:00 +0530

    Create rotate-matrix.cpp

---
+++ Arrays/rotate-matrix.cpp
@@ -0,0 +1,17 @@
+void Solution::rotate(vector<vector<int> > &A) {
+    int n = A.size();
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            swap(A[i][j],A[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n/2;j++)
+        {
+            swap(A[i][j],A[i][n-1-j]);
+        }
+    }
+}
