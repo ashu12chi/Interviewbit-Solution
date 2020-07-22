c00c61a2ff7761772c38e66b64584258f47d50e0
Ashutosh Chitranshi <45908393+ashu12chi@users.noreply.github.com>
2020-07-22 18:24:27 +0530

    Create longest-common-subsequence.cpp

---
+++ Dynamic Programming/longest-common-subsequence.cpp
@@ -0,0 +1,16 @@
+#define ll long long
+ll dp[1001][1001];
+ll solve1(string& A,string& B,int i,int j)
+{
+    if(i == A.size() || j == B.size())
+        return 0;
+    if(dp[i][j] != -1)
+        return dp[i][j];
+    if(A[i] == B[j])
+        return dp[i][j] = 1 + solve1(A,B,i+1,j+1);
+    return dp[i][j] = max(solve1(A,B,i,j+1),solve1(A,B,i+1,j));
+}
+int Solution::solve(string A, string B) {
+    memset(dp,-1ll,sizeof(dp));
+    return solve1(A,B,0,0);
+}
