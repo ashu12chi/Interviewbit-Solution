1cc17fe1331c478eacc548df76a0f51e431ad278
Ashutosh Chitranshi <45908393+ashu12chi@users.noreply.github.com>
2020-08-10 12:30:25 +0530

    Create fizzbuzz.cpp

---
+++ Math/fizzbuzz.cpp
@@ -0,0 +1,23 @@
+vector<string> Solution::fizzBuzz(int A) {
+    vector<string> v;
+    for(int i=1;i<=A;i++)
+    {
+        if(i%3 ==0 && i%5 == 0)
+        {
+            v.push_back("FizzBuzz");
+        }
+        else if(i%3 == 0)
+        {
+            v.push_back("Fizz");
+        }
+        else if(i%5 == 0)
+        {
+            v.push_back("Buzz");
+        }
+        else
+        {
+            v.push_back(to_string(i));
+        }
+    }
+    return v;
+}
